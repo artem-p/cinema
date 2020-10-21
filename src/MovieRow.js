5006c59086a5ff98d1d508b7e508d5442943b47d
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-21 18:08:11 +0300

    add title prop to movie row

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,1 +1,13 @@
 import './MovieRow.css';
+
+import React from 'react'
+
+function MovieRow({ title }) {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+}
+
+export default MovieRow
