1de4fa73496b17bfb8b923012920936c6dde5030
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-23 12:16:47 +0300

    display movies

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './MovieRow.css';
 import api from './axios';
 
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original/";
 
 function MovieRow({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
@@ -10,16 +11,23 @@ function MovieRow({ title, fetchUrl }) {
         // if [], run once when loads and don't run again
         async function fetchData() {
             const request = await api.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results);
+
             return request;
         }
 
         fetchData();
-    }, [])
+    }, [fetchUrl])
 
     return (
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2>
+
+            <div className="movieRow__posters">
+                {movies.map(movie => (
+                    <img key={movie.id} className='movieRow__poster' src={`${BASE_IMAGE_URL}${movie.poster_path}`} alt={movie.name}></img>
+                ))}
+            </div>
         </div>
     )
 }
