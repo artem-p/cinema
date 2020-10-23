8926e1013c44c0c959c14a00efbad01bb6999e03
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-23 11:13:38 +0300

    fetch movies

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,13 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import './MovieRow.css';
+import api from './axios';
 
 
-function MovieRow({ title }) {
+function MovieRow({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         // if [], run once when loads and don't run again
-        
+        async function fetchData() {
+            const request = await api.get(fetchUrl);
+            console.log(request);
+            return request;
+        }
+
+        fetchData();
     }, [])
 
     return (
