96b05da8fc0d3505567d3dd6534559d51e8d115b
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-26 12:02:51 +0300

    add more movies

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
     <div className="App">
       <MovieRow title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
       <MovieRow title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <MovieRow title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <MovieRow title="Action Moview" fetchUrl={requests.fetchActionMovies} />
+      <MovieRow title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <MovieRow title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <MovieRow title="Family Movies" fetchUrl={requests.fetchFamilyMovies} />
+      <MovieRow title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
