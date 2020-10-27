f5a4d21b0e0167ea9cd845336e5f383cfad40102
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-27 15:41:42 +0300

    fetch movie in banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Banner.css';
+import api from './axios';
+import requests from './requests';
+
 
 function Banner() {
-    return (
-        <div>
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchMovie() {
+            const request = await api.get(requests.fetchTrending);
+
+            const randomIndex = Math.floor(Math.random * request.data.results.length);
+            const randomMovie = request.data.results[randomIndex];
             
-        </div>
+            setMovie(randomMovie);
+        }
+
+        fetchMovie();
+    })
+
+    return (
+        <header>
+            
+        </header>
     )
 }
 
