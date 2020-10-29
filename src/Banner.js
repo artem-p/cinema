e863778ca1da60c110c18af9a182bc7035e9729a
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-29 11:47:25 +0300

    add buttons and description

--- src/Banner.js
+++ src/Banner.js
@@ -31,13 +31,22 @@ function Banner() {
                     "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
                 )`,
                 backgroundPosition: "center center"
-            }}
-        >
+            }}>
+                
             <div className="banner__contents">
                 <h1>
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
             </div>
+
+            <div className="banner__buttons">
+                <button className="banner__button">Play</button>
+                <button className="banner__button">My List</button>
+            </div>
+
+            <h1 className="banner__description">
+                {movie?.overview}
+            </h1>
         </header>
     )
 }
