a19a974b466ea25aebbf194d73e331c56286fabe
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-30 12:56:38 +0300

    add logos to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,18 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div>
-                        
+        <div className="navbar">
+            <img
+                className="nav__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix Logo"
+            />
+
+            <img
+                className="nav__avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="Netflix Avatar"
+            />
         </div>
     )
 }
