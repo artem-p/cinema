fe0c2a5044a5859098990d77b614e54140c24875
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-10 14:55:13 +0300

    add trailer urls

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,11 +1,22 @@
 import React, {useState, useEffect} from 'react';
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import './MovieRow.css';
 import api from './axios';
 
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original/";
 
+const YOUTUBE_OPTIONS = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+        autoplay: 1,
+    },
+};
+
 function MovieRow({ title, fetchUrl, isLargePosters }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("")
 
     useEffect(() => {
         // if [], run once when loads and don't run again
@@ -19,18 +30,36 @@ function MovieRow({ title, fetchUrl, isLargePosters }) {
         fetchData();
     }, [fetchUrl])
 
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            console.log(movie?.name);
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error) => console.log(error));
+        }
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
 
             <div className="movieRow__posters">
                 {movies.map(movie => (
-                    <img key={movie.id} className={`movieRow__poster ${isLargePosters && "movieRow__posterLarge"}`} 
-                        src={`${BASE_IMAGE_URL}${isLargePosters ? movie.poster_path : movie.backdrop_path}`} 
+                    <img key={movie.id} 
+                        className={`movieRow__poster ${isLargePosters && "movieRow__posterLarge"}`} 
+                        src={`${BASE_IMAGE_URL}${isLargePosters ? movie.poster_path : movie.backdrop_path}`}
+                        onClick={() => handleClick(movie)} 
                         alt={movie.name}>
                     </img>
                 ))}
             </div>
+
+            {trailerUrl && <Youtube videoId={"XtMThy8QKqU"} opts={YOUTUBE_OPTIONS}/>}
         </div>
     )
 }
